16a001daf280fe6cb2da6f533ee8ac921554d080
Cansu Arslanalp <79985594+cansu19@users.noreply.github.com>
2023-08-07 16:58:36 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Barinak.Odev.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
